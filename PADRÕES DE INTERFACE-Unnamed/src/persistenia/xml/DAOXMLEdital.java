98339e832d655dfbc0ea23c09b38a8dccf1e9588
Gabriel <gabriel.pb.congo@gmail.com>
2020-10-09 12:35:55 -0300

    Modificação no metodo consultarAnd!

--- PADRÕES DE INTERFACE-Unnamed/src/persistenia/xml/DAOXMLEdital.java
+++ PADRÕES DE INTERFACE-Unnamed/src/persistenia/xml/DAOXMLEdital.java
@@ -1,41 +1,43 @@
 package persistenia.xml;
 
 import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
-
 import model.projetos.Edital;
 
 public class DAOXMLEdital {
-	
-	private HashMap<Long,Edital> persistidos;
+
+	private HashMap<Long, Edital> persistidos;
 
 	private final File arquivoColecao = new File("XMLEdital.xml");
 
 	private final XStream xstream = new XStream(new DomDriver("UTF-8"));
 	private long id = 0;
-	
+
 	public boolean criar(Edital edital) {
-		    this.persistidos = this.carregarXML();
-			id += 1;
-			this.persistidos.put(id, edital);
-			this.salvarXML(persistidos);
-			return true;
+		this.persistidos = this.carregarXML();
+		id += 1;
+		this.persistidos.put(id, edital);
+		this.salvarXML(persistidos);
+		return true;
 	}
 
 	public void remover(long id) {
 		this.persistidos = this.carregarXML();
 		persistidos.remove(id);
 		this.salvarXML(persistidos);
-		
+
 	}
 
 	public boolean atualizar(long id, Edital edital) {
@@ -45,15 +47,27 @@ public class DAOXMLEdital {
 		return true;
 	}
 
-	public Set<Edital> consultarAnd(String atributos, String respectivosValoresAtributos) {
-		return null;
+	public Set<Edital> consultarAnd(String nome, Date dataInicio, Date dataTermino, boolean ativo) {
+		this.persistidos = this.carregarXML();
+		Set<Edital> auxiliar = new HashSet<Edital>();
+
+		for (int i = 0; i < persistidos.size(); i++) {
+			Edital editalAuxiliar = persistidos.get(i);
+			if (editalAuxiliar.getNome().equalsIgnoreCase(nome) && editalAuxiliar.getDataInicio().equals(dataInicio)
+					&& editalAuxiliar.getDataTermino().equals(dataTermino) && editalAuxiliar.getAtivo() == ativo) {
+				auxiliar.add(editalAuxiliar);
+
+			}
+
+		}
+		return auxiliar;
 	}
 
 	public Set<Edital> consultarOr(String[] atributos, String[] respectivosValoresAtributos) {
 		return null;
 	}
 
-	private void salvarXML(HashMap<Long,Edital> persistidos) {
+	private void salvarXML(HashMap<Long, Edital> persistidos) {
 		String xml = xstream.toXML(persistidos);
 
 		try {
@@ -66,16 +80,16 @@ public class DAOXMLEdital {
 		}
 	}
 
-	private HashMap<Long,Edital> carregarXML() {
+	private HashMap<Long, Edital> carregarXML() {
 		if (arquivoColecao.exists()) {
 			try {
 				FileInputStream r = new FileInputStream(arquivoColecao);
-				return (HashMap<Long,Edital>) xstream.fromXML(r);
+				return (HashMap<Long, Edital>) xstream.fromXML(r);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
-		return new HashMap<Long,Edital>();
+		return new HashMap<Long, Edital>();
 	}
-	
+
 }
