a8ad697a958f20eb93e54b0695ef046b95605bb5
ytall <ytall@LAPTOP-FKUV9EE0>
2020-10-11 18:43:52 -0300

    caso de uso 8 feito

--- PADRÕES DE INTERFACE-Unnamed/src/model/casosDeUsofachadas/RelatorioFachada.java
+++ PADRÕES DE INTERFACE-Unnamed/src/model/casosDeUsofachadas/RelatorioFachada.java
@@ -1,19 +1,28 @@
 package model.casosDeUsofachadas;
 
-public class RelatorioFachada {
-	public void gerarRelatorio() {
-		long tStart, tEnd, tResult;
-	       StringBuffer strBuffer = new StringBuffer();
-	       tStart = System.currentTimeMillis();
-	       for(int i = 0; i < 100000; i ++){
-	              strBuffer.append("a");
-	       }
-	       tEnd = System.currentTimeMillis();
-	       tResult = tEnd - tStart;
-	       System.out.println("Tempo de Execução com StringBuffer ="+tResult+" ms");
+import model.projetos.Edital;
+import persistenia.xml.DAOXMLEdital;
 
-	}
-	public static void main(String[] args) {
-		new RelatorioFachada().gerarRelatorio();
-	}
+//caso de uso 8
+public class RelatorioFachada {
+	private DAOXMLEdital daoEdital;
+
+    public void gerarRelatorio() {
+        StringBuffer texto = new StringBuffer();
+        texto.append(String.format("<html>\n<head>\n<title>Relatorio de custos</title>\n</head>\n<body>\n<h1 style='text-align:center'>Relatorio</h1>\n"));
+        String[] consulta = { "*" };
+        try {
+            for (Edital edital : daoEdital.consultarAnd(consulta, consulta)) {
+                texto.append(String.format(
+                        "<span>Dados do edital %s -- Valor total de custeio não gasto %s -- valor total de capital não gasto %s</span><br>\n",
+                        edital.getNome(), edital.getCusteioReaisNaoGastoTotal(), edital.getCapitalReaiNaoGastoTotal()));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+                texto.append(String.format("<span>Ainda não à nenhuma valor estabelecido!</span><br>\n"));
+        }
+        System.out.println(texto.append("</body>\n</html>"));
+
+    }
 }
+
