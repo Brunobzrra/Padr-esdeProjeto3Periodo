2cb9084b0d91f26e8bcb15bfc5b49505e146b9cd
Antônio Amorim <Antnio Amorim@10.0.0.111>
2020-11-06 18:56:03 -0200

    mais erros corrigidos

--- PADRÕES DE INTERFACE-Unnamed/src/model/autenticacao/ContaEmail.java
+++ PADRÕES DE INTERFACE-Unnamed/src/model/autenticacao/ContaEmail.java
@@ -3,18 +3,22 @@ package model.autenticacao;
 import java.io.Serializable;
 
 /**
- * Essa classe responsaveis por fazer uso da ponte para variação de tratamento de contas
+ * Essa classe responsaveis por fazer uso da ponte para variação de tratamento
+ * de contas
+ * 
  * @author Antônio Amorim
  *
  */
-public abstract class ContaEmail implements Serializable{
+public abstract class ContaEmail implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private ContaBridge conta;
+
 	/**
 	 * Todas subclasses tem sua propria operações de tratamento de conta
+	 * 
 	 * @param login
 	 * @param senha
 	 * @return
@@ -28,13 +32,17 @@ public abstract class ContaEmail implements Serializable{
 	}
 
 	public boolean equals(ContaEmail compara) {
-		if (conta.getLogin().equals(compara.getConta().getLogin())) {
+		if (conta.getLogin().equals(compara.getLogin())) {
 			return true;
 		}
 		return false;
 	}
 
-	public ContaBridge getConta() {
-		return conta;
+	public String getLogin(){
+		return conta.getLogin();
 	}
+	/*
+	 * TODO 1. Desnecessario no bridge ter um get para implementacao setada nele, no
+	 * geral.
+	 */
 }
