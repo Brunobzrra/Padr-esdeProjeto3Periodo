adb84325f5c67ae091a14ad7ea452f2eeede580c
bruno <bruno.bzrrasantos@gmail.com>
2020-11-09 14:55:48 -0200

    Pequena modificação para verificar se o membro esta online no caso de uso 7

--- PADRÕES DE INTERFACE-Unnamed/src/model/casosDeUsofachadas/CasoDeUsoSete.java
+++ PADRÕES DE INTERFACE-Unnamed/src/model/casosDeUsofachadas/CasoDeUsoSete.java
@@ -3,6 +3,7 @@ package model.casosDeUsofachadas;
 import java.util.Set;
 
 import model.autenticacao.Membro;
+import model.autenticacao.RegistradorSessaoLogin;
 import persistencia.xml.DAOXMLMembroConta;
 
 //caso de uso 7
@@ -16,21 +17,24 @@ public class CasoDeUsoSete {
 
 	public void habilitarAdministrador(long matricula) throws Exception {
 		if (administrador != null) {
-			String[] atributos = { "matricula" };
-			Object[] valores = { matricula };
-			Set<Membro> membro = daoMembro.consultarAnd(atributos, valores);
-			try {
-				for (Membro membroModificado : membro) {
-					if (!administrador.equals(membroModificado)) {
-						Membro membroAntigo = membroModificado;
-						membroModificado.setAdministrador(true);
-						daoMembro.atualizar(membroAntigo, membroModificado);
-					} else {
-						throw new Exception("Você não pode se auto-desabilitar do cargo, contate outro administrador!");
+			if (RegistradorSessaoLogin.getInstance().isOline(administrador.getEmail())) {
+				String[] atributos = { "matricula" };
+				Object[] valores = { matricula };
+				Set<Membro> membro = daoMembro.consultarAnd(atributos, valores);
+				try {
+					for (Membro membroModificado : membro) {
+						if (!administrador.equals(membroModificado)) {
+							Membro membroAntigo = membroModificado;
+							membroModificado.setAdministrador(true);
+							daoMembro.atualizar(membroAntigo, membroModificado);
+						} else {
+							throw new Exception(
+									"Você não pode se auto-desabilitar do cargo, contate outro administrador!");
+						}
 					}
+				} catch (Exception e) {
+					throw new Exception("Não existe membro com essa matricula!");
 				}
-			} catch (Exception e) {
-				throw new Exception("Não existe membro com essa matricula!");
 			}
 		} else {
 			throw new Exception("Você não é administrador");
